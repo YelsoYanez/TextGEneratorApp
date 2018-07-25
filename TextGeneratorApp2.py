f1c1fe5e0c4cbac029997c7f37a0c2e9aac98ecd
Yelso Yanez <yyelso@yahoo.com>
2018-07-25 11:15:19 -0400

    stashing these changes

--- TextGeneratorApp2.py
+++ TextGeneratorApp2.py
@@ -11,32 +11,22 @@ Linput_1 = input("What letter do you want? 'v' for vowels, 'c' for consonants, '
 Linput_2 = input("What letter do you want? 'v' for vowels, 'c' for consonants, 'l' for any letters: ")
 Linput_3 = input("What letter do you want? 'v' for vowels, 'c' for consonants, 'l' for any letters: ")
 
-
 def generator():
-    if Linput_1 == 'v':
-        letter1  = random.choice(vowels)
-    elif Linput_1 == 'c':
-        letter1  = random.choice(consonants)
-    elif Linput_1 == 'l':
-        letter1  = random.choice(letters)
+    if Linput_1 == 'v': letter1  = random.choice(vowels)
+    elif Linput_1 == 'c': letter1  = random.choice(consonants)
+    elif Linput_1 == 'l': letter1  = random.choice(letters)
     else:
         letter1 = Linput_1
 
-    if Linput_2 == 'v':
-        letter2  = random.choice(vowels)
-    elif Linput_2 == 'c':
-        letter2  = random.choice(consonants)
-    elif Linput_2 == 'l':
-        letter2  = random.choice(letters)
+    if Linput_2 == 'v': letter2  = random.choice(vowels)
+    elif Linput_2 == 'c': letter2  = random.choice(consonants)
+    elif Linput_2 == 'l': letter2  = random.choice(letters)
     else:
         letter2 = Linput_2
 
-    if Linput_3 == 'v':
-        letter3  = random.choice(vowels)
-    elif Linput_3 == 'c':
-        letter3  = random.choice(consonants)
-    elif Linput_3 == 'l':
-        letter3  = random.choice(letters)
+    if Linput_3 == 'v': letter3  = random.choice(vowels)
+    elif Linput_3 == 'c': letter3  = random.choice(consonants)
+    elif Linput_3 == 'l': letter3  = random.choice(letters)
     else:
         letter3 = Linput_3
 
